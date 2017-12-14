5af89455d7f6abeb4535038563ebdb9b1b5739a5
Henrique Silva Fernandes <up201106591@fc.up.pt>
2017-12-14 14:41:55 +0000

    install system updated

--- vmdStore/lib/install.tcl
+++ vmdStore/lib/install.tcl
@@ -3,87 +3,110 @@ package provide vmdStoreInstallPlugins 0.1
 proc vmdStore::installPlugin {plugin} {
     vmdStore::installGui $plugin
 
-    set path "$vmdStore::server/plugins/$plugin"
-    set installPath "$::vmdStorePath/plugins"
-
-    set fileName ""
-    set fileName [append fileName $plugin "_V" $vmdStore::pluginVersion]
-
-    ## Download Plugin
-    vmdhttpcopy "$path/$fileName.tar" "$::vmdStorePath/temp/plugin.tar"
-
-    ## Untar the plugin
-    file delete -force "$::vmdStorePath/plugins/$plugin"
-    ::tar::untar "$::vmdStorePath/temp/plugin.tar" -dir $installPath
-
-
-    ## Download VMDRC information to install
-    vmdhttpcopy "$path/vmdrc.txt" "$::vmdStorePath/temp/vmdrc.txt"
-
-
-    set vmdrcFile [open "$::vmdStorePath/temp/vmdrc.txt" r]
-    set vmdrcFileContent [read $vmdrcFile]
-    close $vmdrcFile
-
-    set initDelimiter ""
-    set finalDelimiter ""
-
-    foreach line [split $vmdrcFileContent "\n"] {
-        if {[regexp "####vmdStore#### START" $line] == 1} {
-            set initDelimiter $line
-        } elseif {[regexp "####vmdStore#### END" $line] == 1} {
-            set finalDelimiter $line
-        }
-    }
-    
-    if {[string first "Windows" $::tcl_platform(os)] != -1} {
-		set vmdrcPath "./vmd.rc"
+    #### Save a backup of vmdrc
+	if {[string first "Windows" $::tcl_platform(os)] != -1} {
+		catch {file copy -force "$env(HOME)/vmd.rc" "$env(HOME)/vmd.rc.bak.vmdStore"}
 	} else {
-		set vmdrcPath "~/.vmdrc"
+		catch {file copy -force ~/.vmdrc ~/.vmdrc.bak.vmdStore}
 	}
 
-    set vmdrcLocal [open $vmdrcPath r]
-    set vmdrcLocalContent [split [read $vmdrcLocal] "\n"]
-    close $vmdrcLocal
+    # Getting the local Version of the Plugin
+	set localVersion [lindex [lindex $vmdStore::installedPlugins [lsearch -index 0 $vmdStore::installedPlugins "$plugin"]] 1]
 
-    file delete -force $vmdrcPath
-    set vmdrcLocal [open $vmdrcPath w]
+    # Getting the online version of the Plugin
+    set onlineVersion $vmdStore::pluginVersion
 
-    set printOrNot 1
-    set printOrNotA 0
-    set i 0
-    foreach line $vmdrcLocalContent {
-        if {[regexp $initDelimiter $line] == 1} {
-            set printOrNot 0
-        } elseif {[regexp $finalDelimiter $line] == 1} {
-            set printOrNotA 1
-        }
 
-        if {$printOrNot == 1 && $line != ""} {
-            puts $vmdrcLocal $line
-        }
+    ###### Installing the Plugin
+	puts "The most recent version of $plugin is installing..."
 
-        if {$printOrNotA == 1} {
-            set printOrNot 1
-        }
+    # Cleaning the temp directory
+    catch {file delete -force "$::vmdStorePath/temp"}
+	file mkdir "$::vmdStorePath/temp"
 
-        incr i
-    }
-    
-    set i 0
-    foreach line [split $vmdrcFileContent "\n"] {
-        if {[regexp "none" $line] == 1} {
-            tk_messageBox -title "VMD Store" -icon warning -message "$plugin require some configurations." -detail "[string range [lindex [split $vmdrcFileContent "\n"] [expr $i - 1]] 1 end]"
-            set path [tk_chooseDirectory]
-            regexp {(.*.) none} $line -> newLine
-            puts $vmdrcLocal "$newLine \"$path\""
-        } else {
-            puts $vmdrcLocal $line
-        }
-        incr i
-    }
+    set url "https://github.com/portobiocomp/$plugin/archive/$onlineVersion.zip"
+	set token [::http::geturl $url -timeout 30000]
+	set data [::http::data $token]
+	regexp -all {href=\"(\S+)\"} $data --> url
+	puts "Downloading the plugin from: $url"
+	set outputFile  [open "$::vmdStorePath/temp/plugin.zip" w]
+	set token [::http::geturl $url -channel $outputFile -timeout 1800000 -progress vmdStoreDownlodProgress]
+    close $outputFile
 
-    close $vmdrcLocal
+    # Extracting the plugin
+    if {[string first "Windows" $::tcl_platform(os)] != -1} {
+			
+	} else {
+		exec unzip -q -o "$::vmdStorePath/temp/plugin.zip" -d "$::vmdStorePath/temp"
+	}
+
+    # Copy Files
+	vmdStoreCopyFiles "$::vmdStorePath/temp/$plugin-$onlineVersion/$plugin" "$::vmdStorePath/plugins/$plugin"
+
+
+    ##### Update VMDRC file
+		set vmdrcFile [open "$::vmdStorePath/temp/$plugin-$onlineVersion/install.txt" r]
+    	set vmdrcFileContent [read $vmdrcFile]
+    	close $vmdrcFile
+		if {[string first "Windows" $::tcl_platform(os)] != -1} {
+			set vmdrcPath "$env(HOME)/vmd.rc"
+		} else {
+			set vmdrcPath "~/.vmdrc"
+		}
+
+		foreach line [split $vmdrcFileContent "\n"] {
+    	    if {[regexp "####vmdStore#### START" $line] == 1} {
+    	        set initDelimiter $line
+    	    } elseif {[regexp "####vmdStore#### END" $line] == 1} {
+    	        set finalDelimiter $line
+    	    }
+    	}
+
+		set vmdrcLocal [open $vmdrcPath r]
+	    set vmdrcLocalContent [split [read $vmdrcLocal] "\n"]
+	    close $vmdrcLocal
+
+	    file delete -force $vmdrcPath
+	    set vmdrcLocal [open $vmdrcPath w]
+
+		set printOrNot 1
+	    set printOrNotA 0
+	    set i 0
+	    foreach line [split $vmdrcFileContent "\n"] {
+	        if {[regexp "none" $line] == 1} {
+	            tk_messageBox -title "VMD Store" -icon warning -message "$plugin require some configurations." -detail "[string range [lindex [split $vmdrcFileContent "\n"] [expr $i - 1]] 1 end]"
+                set path [tk_chooseDirectory]
+	            regexp {(.*.) none} $line -> newLine
+	            puts $vmdrcLocal "$newLine \"$path\""
+	        } elseif {[regexp "XXversionXX" $line] == 1} {
+				regexp {(.*.) XXversionXX} $line -> newLine
+				puts $vmdrcLocal "$newLine $onlineVersion"
+			} else {
+	            puts $vmdrcLocal $line
+	        }
+	    }
+
+	    foreach line $vmdrcLocalContent {
+	        if {[regexp $initDelimiter $line] == 1} {
+	            set printOrNot 0
+	        } elseif {[regexp $finalDelimiter $line] == 1} {
+	            set printOrNotA 1
+	        }
+
+	        if {$printOrNot == 1 && $line != ""} {
+	            puts $vmdrcLocal $line
+	        }
+
+	        if {$printOrNotA == 1} {
+	            set printOrNot 1
+	        }
+
+	        incr i
+	    }
+	
+
+	    close $vmdrcLocal
+
 
     destroy $::vmdStore::installing
     tk_messageBox -title "VMD Store" -icon info -message "$plugin was installed sucessfully!" -detail "Please, restart VMD to apply the new settings."
